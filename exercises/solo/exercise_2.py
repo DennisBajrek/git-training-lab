bcc47dcb8366277e9463c5126723e0c81c0e202c
DennisBajrek <Dennis.Najetovic@gmail.com>
2026-01-28 16:54:37 +0100

    Övning 2: Lägg till Buzz för tal som kan divideras med 5

--- exercises/solo/exercise_2.py
+++ exercises/solo/exercise_2.py
@@ -45,6 +45,8 @@ def fizzbuzz(n: int) -> list:
         for i in range(1, n + 1):
             if i % 3 == 0:
                 result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
             else:
                 result.append(str(i))
             return result
